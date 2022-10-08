7991a0f154bf7e5d7a843e55f6483e84689e2af9
Amir Basirati <amirbasirati13@gmail.com>
2022-10-08 15:03:52 +0330

    project created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios'
+import { useState } from 'react';
+import "./App.css"
 
 function App() {
+
+  const [quotes, setQuotes] = useState("")
+  
+  useEffect( () => {
+    const getQuote = async() => {
+      const response = await axios.get('https://quotes.rest/qod')
+      setQuotes(response.data.contents.quotes[0])
+      console.log(response)
+    }
+    getQuote()
+  }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote-box">
+        <h1>Today's Quote</h1>
+          <h4>{quotes.quote}</h4>
+          <h6>-{quotes.author}-</h6>
+      </div>
     </div>
   );
 }
+          
 
 export default App;
